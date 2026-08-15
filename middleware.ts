5d815111c4964ec01e509d9b2cce386ff5d4fb92
nexo8525-dev <nexo8525@gmail.com>
2026-08-15 14:09:01 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,9 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!_next|favicon.ico).*)'],
+  matcher: [
+    '/dashboard(.*)',
+    '/onboarding(.*)',
+    '/api/(.*)',
+  ],
 };
